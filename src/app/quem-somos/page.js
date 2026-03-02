a00fb6bfddfca898266acc4d90fedfa257146125
Hyan Ferreira <hcgamesmg2016@gmail.com>
2026-03-02 19:53:34 -0300

    Update: page name for Portuguese

--- src/app/quem-somos/page.js
+++ src/app/quem-somos/page.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import styles from "../page.module.css";
 
-export default function AboutUs() {
+export default function QuemSomos() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
